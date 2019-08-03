36e4102b00b28194e46601e96624a1794d651a08
MateuszKalus <51907460+MateuszKalus@users.noreply.github.com>
2019-08-03 12:50:17 +0200

    03.08 update

---
+++ repos/377_dp/377_dp/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _377_dp
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int ilerazy = Int32.Parse(Console.ReadLine());
+
+            string[] datas = new string[1000];
+            for (int i = 0; i < ilerazy; i++)
+            {
+                Analizator analizator = new Analizator();
+                Skrzynia skrzynia = new Skrzynia();
+                Pudelko pudelko = new Pudelko();
+                
+                string wej_napis = Console.ReadLine();
+
+                datas = wej_napis.Split(',');
+
+                /*
+                int dlugosctablicy = datas.Length;
+                for (int i = 0; i < datas.Length-1; i++)
+                {
+
+                }
+                */
+                skrzynia.Przypisz(Int32.Parse(datas[0]), Int32.Parse(datas[1]), Int32.Parse(datas[2]));
+                pudelko.Przypisz(Int32.Parse(datas[3]), Int32.Parse(datas[4]), Int32.Parse(datas[5]));
+
+                
+                Console.WriteLine(analizator.Oblicz(skrzynia, pudelko));
+
+                //Console.WriteLine();
+                Console.ReadLine();
+            }
+                                  
+        }
+       
+    }
+
+    class Prostokat
+    {
+
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public int Z { get; private set; }
+
+        public void Przypisz(int xw, int yw, int zw)
+        {
+
+            X = xw;
+            Y = yw;
+            Z = zw;
+        }
+    }
+
+    class Skrzynia : Prostokat
+    {
+
+    }
+
+    class Pudelko : Prostokat
+    {
+
+    }
+
+    class Analizator
+    {
+
+        List<Obiekt> B = new List<Obiekt>();
+        List<int> LastEq = new List<int>();
+
+        public string Oblicz(Skrzynia skrzynia, Pudelko pudelko) 
+        {
+            List<int> A = new List<int> { pudelko.X, pudelko.Y, pudelko.Z };
+            List<int> skrz = new List<int> { skrzynia.X, skrzynia.Y, skrzynia.Z };
+            /*
+            Obiekt[] obiekt = new Obiekt[6];
+
+            obiekt[0] = new Obiekt(pudelko.X, pudelko.Y, pudelko.Z);
+            obiekt[1] = new Obiekt(pudelko.X, pudelko.Z, pudelko.Y);
+            obiekt[2] = new Obiekt(pudelko.Y, pudelko.X, pudelko.Z);
+            obiekt[3] = new Obiekt(pudelko.Y, pudelko.Z, pudelko.X);
+            obiekt[4] = new Obiekt(pudelko.Z, pudelko.Y, pudelko.X);
+            obiekt[5] = new Obiekt(pudelko.Z, pudelko.X, pudelko.Y);
+            */
+            Permutuj(A);
+            for (int i = 0; i < B.Count; i++)
+            {
+
+                LastEq.Add((skrz[0] / B[i].x) * (skrz[1] / B[i].y) * (skrz[2] / B[i].z));
+            }
+
+            //foreach (var arg in B)
+            //{
+            //    Console.WriteLine($"{arg.x} {arg.y} {arg.z}");
+            //}
+            return LastEq.Max().ToString();
+        }
+
+        public void Permutuj(List<int> A)
+        {
+            int a = A.Count;
+
+            if (a == 1)
+            {
+                B.Add(new Obiekt(A));
+             //   foreach (var arg in A)
+             //   {
+             //       Console.WriteLine(arg);
+             //   }
+
+            }
+
+            else
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    int temp;
+                    temp = A[i];
+                    A[i] = A[a - 1];
+                    A[a - 1] = temp;
+
+                    Permutuj(a - 1, A);
+
+                    temp = A[i];
+                    A[i] = A[a - 1];
+                    A[a - 1] = temp;
+                }
+            }
+        }
+
+        public void Permutuj(int a, List<int> A)
+        {
+            if (a == 1)
+            {
+                B.Add(new Obiekt(A));
+
+            }
+
+            else
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    int temp;
+                    temp = A[i];
+                    A[i] = A[a - 1];
+                    A[a - 1] = temp;
+
+                    Permutuj(a - 1, A);
+
+                    temp = A[i];
+                    A[i] = A[a - 1];
+                    A[a - 1] = temp;
+                }
+            }
+        }
+
+        class Obiekt 
+        {
+            public int x, y, z;
+            public Obiekt(List<int> A)
+            {
+                x = A[0];
+                y = A[1];
+                z = A[2];
+            }
+
+        }
+    }
+
+
+
+}
---
+++ repos/test1_WindowsForms/test1_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace test1_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kopiujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wklejToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zgoda_bt = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kopiujToolStripMenuItem,
+            this.wklejToolStripMenuItem,
+            this.zakończToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(119, 70);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // kopiujToolStripMenuItem
+            // 
+            this.kopiujToolStripMenuItem.Name = "kopiujToolStripMenuItem";
+            this.kopiujToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.kopiujToolStripMenuItem.Text = "Kopiuj";
+            // 
+            // wklejToolStripMenuItem
+            // 
+            this.wklejToolStripMenuItem.Name = "wklejToolStripMenuItem";
+            this.wklejToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.wklejToolStripMenuItem.Text = "Wklej";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::test1_WindowsForms.Properties.Resources.mk;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(195, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Wyślij dane";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zgoda_bt
+            // 
+            this.zgoda_bt.AutoSize = true;
+            this.zgoda_bt.Location = new System.Drawing.Point(195, 157);
+            this.zgoda_bt.Name = "zgoda_bt";
+            this.zgoda_bt.Size = new System.Drawing.Size(84, 17);
+            this.zgoda_bt.TabIndex = 3;
+            this.zgoda_bt.Text = "zgadzam się";
+            this.zgoda_bt.UseVisualStyleBackColor = true;
+            this.zgoda_bt.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ContextMenuStrip = this.contextMenuStrip2;
+            this.textBox1.Location = new System.Drawing.Point(42, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(520, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip2;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Location = new System.Drawing.Point(42, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(520, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(42, 271);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(595, 576);
+            this.ContextMenuStrip = this.contextMenuStrip2;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.zgoda_bt);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "CG_DataConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox zgoda_bt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStripMenuItem kopiujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wklejToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
